60f96e9689c680fefed7a4da2a28f5ced52be1d8
Aditi <aditisaggarwins@gmail.comm>
2024-05-10 12:41:15 +0530

    update the env files for amanagement, services,customer

--- apps/management/src/dbConfigure/dbConnection.ts
+++ apps/management/src/dbConfigure/dbConnection.ts
@@ -1,27 +1,21 @@
-import {Sequelize} from 'sequelize';
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
- const sequelize = new Sequelize(
+const sequelize = new Sequelize(process.env.DB_NAME || 'management', process.env.DB_USER || 'root', process.env.DB_PASSWORD || 'Shagun@12345#', {
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'mysql',
+  logging: console.log,
+});
 
-      
-  process.env.DB_NAME || 'management',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || 'Shagun@12345#',
-  {
-      host: process.env.DB_HOST || 'localhost',
-      dialect: 'mysql',
-      logging: console.log 
-  }
-  );
-   
-   sequelize.authenticate().then(() => {
-      console.log('Connection has been established successfully.');
-   }).catch((error) => {
-      console.error('Unable to connect to the database', error);
-   });
-   
-   export default sequelize;  
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database', error);
+  });
 
-   
+export default sequelize;
--- apps/service/src/controller/serviceController.ts
+++ apps/service/src/controller/serviceController.ts
@@ -3,114 +3,99 @@ import { message, statusCode } from '../utils/constants';
 import { successAction, failAction } from '../utils/response';
 import { Request, Response } from 'express';
 import logger from '../utils/logger/index';
-import customer from '../../../customer/src/model/customerModel'
-import user from '../../../management/src/model/userModel';
-
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import customer from '../../../customer/src/model/customerModel';
 
 class serviceController {
-//service create //
+  //service create //
 
-public static async serviceCreate(req: Request, res: Response) {
-  try {
-    const serviceData = req.body;
+  public static async serviceCreate(req: Request, res: Response) {
+    try {
+      const serviceData = req.body;
 
-    // Call the serviceCreate method from your service class
-    const createdService = await service.serviceCreate(serviceData);
+      // Call the serviceCreate method from your service class
+      const createdService = await service.serviceCreate(serviceData);
 
-    // Check if service creation was successful
-    if (!createdService) {
-      return res.status(statusCode.notFound).json(failAction(statusCode.notFound, 'Service not created'));
+      // Check if service creation was successful
+      if (!createdService) {
+        return res.status(statusCode.notFound).json(failAction(statusCode.notFound, 'Service not created'));
+      }
+
+      return res.status(201).json(successAction(statusCode.success, createdService));
+    } catch (err) {
+      logger.error(message.errorLog('create', 'serviceController', err));
+      return res.status(statusCode.internalServerError).json(failAction(statusCode.internalServerError, err.message, message.somethingWrong));
     }
-
-  
-    return res.status(201).json(successAction(statusCode.success, createdService));
-  } catch (err) {
-    
-    logger.error(message.errorLog('create', 'serviceController', err));
-    return res.status(statusCode.internalServerError).json(failAction(statusCode.internalServerError, err.message, message.somethingWrong));
   }
-}
 
+  /*********** getUserCustomerById ************/
 
-/*********** getUserCustomerById ************/
-
-
-public static async getServiceById(req: Request, res: Response) {
-  try {
-    const id: string = req.params.id;
-    if (!id) {
-      return res.status(statusCode.badRequest).json(failAction(statusCode.badRequest, 'service ID is required'));
+  public static async getServiceById(req: Request, res: Response) {
+    try {
+      const id: string = req.params.id;
+      if (!id) {
+        return res.status(statusCode.badRequest).json(failAction(statusCode.badRequest, 'service ID is required'));
+      }
+      const services = await service.getServiceById(id);
+      console.log(services, 'ebvcfgfhgh');
+      if (!services) {
+        return res.status(statusCode.notFound).json(failAction(statusCode.notFound, 'service not found'));
+      }
+      return res.status(statusCode.success).json(successAction(statusCode.success, services));
+    } catch (err) {
+      logger.error(message.errorLog('getServiceById', 'serviceController', err));
+      return res.status(statusCode.internalServerError).json(failAction(statusCode.internalServerError, err.message, message.somethingWrong));
     }
-    const services = await service.getServiceById(id);
-    console.log(services, "ebvcfgfhgh")
-    if (!services) {
-      return res.status(statusCode.notFound).json(failAction(statusCode.notFound, 'service not found'));
-    }
-    return res.status(statusCode.success).json(successAction(statusCode.success, services));
-  } catch (err) {
-    logger.error(message.errorLog('getServiceById', 'serviceController', err));
-    return res.status(statusCode.internalServerError).json(failAction(statusCode.internalServerError, err.message, message.somethingWrong));
   }
-}
 
+  public static async serviceBooking(req: Request, res: Response) {
+    try {
+      const servicedata: any = req.body;
+      const { customerid, serviceid, dateTime } = servicedata;
 
-public static async serviceBooking(req: Request, res: Response) {
-  try {
-    const servicedata: any = req.body;
-    const { customerid, serviceid, dateTime } = servicedata;
+      if (!customerid || !serviceid || !dateTime) {
+        return res.status(statusCode.badRequest).json(failAction(statusCode.badRequest, 'customerid, serviceid, and dateTime are required'));
+      }
 
-   
-    if (!customerid || !serviceid || !dateTime) {
-      return res.status(statusCode.badRequest).json(failAction(statusCode.badRequest, 'customerid, serviceid, and dateTime are required'));
+      const customerExists = await customer.findByPk(customerid);
+      if (!customerExists) {
+        return res.status(statusCode.notFound).json(failAction(statusCode.notFound, 'Customer not found'));
+      }
+
+      const serviceExists = await service.findOne({ where: { id: serviceid } });
+      if (!serviceExists) {
+        return res.status(statusCode.notFound).json(failAction(statusCode.badRequest, 'Service not found'));
+      }
+
+      // Call service to create booking
+      const booking = await service.serviceBooking(servicedata);
+
+      // Return success response
+      return res.status(statusCode.success).json(successAction(statusCode.success, 'serviceBooking created successfully'));
+    } catch (err) {
+      logger.error(message.errorLog('serviceBooking', 'serviceController', err));
+      return res.status(statusCode.internalServerError).json(failAction(statusCode.internalServerError, err.message, message.somethingWrong));
     }
-
-    
-    const customerExists = await customer.findByPk(customerid);
-    if (!customerExists) {
-      return res.status(statusCode.notFound).json(failAction(statusCode.notFound, 'Customer not found'));
-    }
-
-  
-    const serviceExists = await service.findOne({ where: { id: serviceid } });
-    if (!serviceExists) {
-      return res.status(statusCode.notFound).json(failAction(statusCode.badRequest, 'Service not found'));
-    }
-
-    // Call service to create booking
-    const booking = await service.serviceBooking(servicedata);
-
-    // Return success response
-    return res.status(statusCode.success).json(successAction(statusCode.success, 'serviceBooking created successfully',));
-
-  } catch (err) {
-    logger.error(message.errorLog('serviceBooking', 'serviceController', err));
-    return res.status(statusCode.internalServerError).json(failAction(statusCode.internalServerError, err.message, message.somethingWrong));
   }
-}
 
+  public static async assignServiceToStaff(req: Request, res: Response) {
+    try {
+      const { staffid, serviceid } = req.body;
 
-public static async assignServiceToStaff(req: Request, res: Response) {
-  try {
-    const { staffid, serviceid } = req.body;
+      // Check if staffId and taskId are provided
+      if (!staffid || !serviceid) {
+        return res.status(400).json({ error: 'Staff ID and service iD are required' });
+      }
 
-    // Check if staffId and taskId are provided
-    if (!staffid || !serviceid) {
-      return res.status(400).json({ error: 'Staff ID and service iD are required' });
+      // Assign service to staff using service
+      const result = await service.assignServiceToStaff(staffid, serviceid);
+
+      return res.json({ message: 'Service assigned successfully', result });
+    } catch (error) {
+      console.error('Error assigning service:', error);
+      return res.status(500).json({ error: 'Internal server error' });
     }
-
-    // Assign service to staff using service
-    const result = await service.assignServiceToStaff(staffid, serviceid);
-    
-    return res.json({ message: 'Service assigned successfully', result });
-  } catch (error) {
-    console.error('Error assigning service:', error);
-    return res.status(500).json({ error: 'Internal server error' });
   }
-}
-
-
-
-
 }
 
 export default serviceController;
